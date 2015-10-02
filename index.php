e448bdd6ae092f5ab10b9f8a8dea62137502906f
elmo911 <jacobmwolfe@gmail.com>
2015-10-02 12:41:05 -0500

    DB write

--- index.php
+++ index.php
@@ -33,11 +33,32 @@ $db = "acsm_0e7ddff7b7d920f";
 try {
     $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    echo "<p> connected </p>";
+    //echo "<p> connected </p>";
 }
 catch(Exception $e){
     die(var_dump($e));
-    echo "<p> not connected... </p>";
+    //echo "<p> not connected... </p>";
+}
+
+// Add to DB
+if(!empty($_POST)) {
+try {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $date = date("Y-m-d");
+    // Insert data
+    $sql_insert = "INSERT INTO registration_tbl (name, email, date)
+                   VALUES (?,?,?)";
+    $stmt = $conn->prepare($sql_insert);
+    $stmt->bindValue(1, $name);
+    $stmt->bindValue(2, $email);
+    $stmt->bindValue(3, $date);
+    $stmt->execute();
+}
+catch(Exception $e) {
+    die(var_dump($e));
+}
+echo "<h3>Your're registered!</h3>";
 }
 ?>
 </body>
