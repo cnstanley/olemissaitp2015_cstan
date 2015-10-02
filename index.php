a4064d02360d380ee0a2e066b8f11519d413ebe8
mpgoodwin <mpgoodwi@go.olemiss.edu>
2015-10-02 14:49:37 -0500

    first push

--- index.php
+++ index.php
@@ -27,34 +27,31 @@
     <input type="text" name="email" id="email">
   </fieldset>
   <fieldset><legend>Competitions</legend>
-    <input type="checkbox" name="competions" value="ncc"> National Collegiate Competition<br>
-    <input type="checkbox" name="competions" value="regionals"> Regionals<br>
+    <input type="checkbox" name="check_list[]" value="National Collegiate Competition"> National Collegiate Competition<br>
+    <input type="checkbox" name="check_list[]" value="Regionals"> Regionals<br>
   </fieldset>
   <fieldset><legend>Focus Groups</legend>
-    <input type="checkbox" name="groups" value="business analytics"> Business Analytics<br>
-    <input type="checkbox" name="groups" value="database design"> Database Design<br>
-    <input type="checkbox" name="groups" value="microsoft office"> Microsoft Office<br>
-    <input type="checkbox" name="groups" value="mobil applications"> Mobile Applications<br>
-    <input type="checkbox" name="groups" value="network design"> Network Design<br>
-    <input type="checkbox" name="groups" value="pc troubleshooting"> PC Troubleshooting/Support<br>
-    <input type="checkbox" name="groups" value="security"> Security<br>
-    <input type="checkbox" name="groups" value="system analysis"> System Analysis & Design<br>
+    <input type="checkbox" name="check_list[]" value="Database Design"> Database Design<br>
+    <input type="checkbox" name="check_list[]" value="Microsoft Office"> Microsoft Office<br>
+    <input type="checkbox" name="check_list[]" value="Mobile Applications"> Mobile Applications<br>
+    <input type="checkbox" name="check_list[]" value="Network Design"> Network Design<br>
+    <input type="checkbox" name="check_list[]" value="PC Troubleshooting/Support"> PC Troubleshooting/Support<br>
+    <input type="checkbox" name="check_list[]" value="Security"> Security<br>
+    <input type="checkbox" name="check_list[]" value="System Analysis & Design"> System Analysis & Design<br>
+  </fieldset>
+  <fieldset><legend>Other</legend>
+    <input type="checkbox" name="check_list[]" value="Website"> Website<br>
   </fieldset>
   <input type="submit" name="submit" value="Submit">
 </form>
 <?php
-include 'registration-class.php';
-
-$Registration = new Registration();
-
-$Registration->connectToDB();
-
-// Add to DB
-if(!empty($_POST)) {
-$Registration->addToDB();
+if(!empty($_POST['check_list'])) {
+    foreach($_POST['check_list'] as $check) {
+            echo $check; //echoes the value set in the HTML form for each checked checkbox.
+                         //so, if I were to check 1, 3, and 5 it would echo value 1, value 3, value 5.
+                         //in your case, it would echo whatever $row['Report ID'] is equivalent to.
+    }
 }
-
-$Registration->showRegistrants();
 ?>
 </body>
 </html>
