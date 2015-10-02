d4f036ebbb7a3ecfcea1af79bb2fe23c7043e59d
mpgoodwin <mpgoodwi@go.olemiss.edu>
2015-10-02 14:12:52 -0500

    example

--- action.php
+++ action.php
@@ -1,45 +1,22 @@
 <html>
 <title>Action</title>
 
-<form method="POST" action="action.php" enctype="multipart/form-data">
-  <fieldset><legend>Personal information</legend>
-    Name:<br>
-    <input type="text" name="name"  id="name">
-    <br>
-    Email:<br>
-    <input type="text" name="email" id="email">
-  </fieldset>
-  <fieldset><legend>Competitions</legend>
-    <input type="checkbox" name="competion[]" id="competition" value="ncc"> National Collegiate Competition<br>
-    <input type="checkbox" name="competion[]" id="competition" value="regionals"> Regionals<br>
-  </fieldset>
-  <fieldset><legend>Focus Groups</legend>
-    <input type="checkbox" name="group[]" id="group" value="business analytics"> Business Analytics<br>
-    <input type="checkbox" name="group[]" id="group" value="database design"> Database Design<br>
-    <input type="checkbox" name="group[]" id="group" value="microsoft office"> Microsoft Office<br>
-    <input type="checkbox" name="group[]" id="group" value="mobile applications"> Mobile Applications<br>
-    <input type="checkbox" name="group[]" id="group" value="network design"> Network Design<br>
-    <input type="checkbox" name="group[]" id="group" value="pc troubleshooting"> PC Troubleshooting/Support<br>
-    <input type="checkbox" name="group[]" id="group" value="security"> Security<br>
-    <input type="checkbox" name="group[]" id="group" value="system analysis"> System Analysis & Design<br>
-  </fieldset>
-  <input type="submit" name="submit" value="Submit">
+<form action="test.php" method="post">
+<input type="checkbox" name="check_list[]" value="value 1">
+<input type="checkbox" name="check_list[]" value="value 2">
+<input type="checkbox" name="check_list[]" value="value 3">
+<input type="checkbox" name="check_list[]" value="value 4">
+<input type="checkbox" name="check_list[]" value="value 5">
+<input type="submit" />
 </form>
-
 <?php
-
-$name = $_POST['competion'];
-
-if(isset($_POST['competition'])) {
-  echo 'you chose:<br />';
-  foreach ($name as $c){
-  echo $c."<br />";
-
-  }
+if(!empty($_POST['check_list'])) {
+    foreach($_POST['check_list'] as $check) {
+            echo $check; //echoes the value set in the HTML form for each checked checkbox.
+                         //so, if I were to check 1, 3, and 5 it would echo value 1, value 3, value 5.
+                         //in your case, it would echo whatever $row['Report ID'] is equivalent to.
+    }
 }
-else {
-  echo 'error getting competitions';
-}
- ?>
+?>
 
 </html>
