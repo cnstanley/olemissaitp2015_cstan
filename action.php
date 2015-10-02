c7ec3eaa0e8896d18db383a0a959727df5b6beae
mpgoodwin <mpgoodwi@go.olemiss.edu>
2015-10-02 14:05:17 -0500

    testing foreach

--- action.php
+++ action.php
@@ -1,12 +1,37 @@
 <html>
 <title>Action</title>
 
+<form method="POST" action="action.php" enctype="multipart/form-data">
+  <fieldset><legend>Personal information</legend>
+    Name:<br>
+    <input type="text" name="name"  id="name">
+    <br>
+    Email:<br>
+    <input type="text" name="email" id="email">
+  </fieldset>
+  <fieldset><legend>Competitions</legend>
+    <input type="checkbox" name="competion[]" id="competition" value="ncc"> National Collegiate Competition<br>
+    <input type="checkbox" name="competion[]" id="competition" value="regionals"> Regionals<br>
+  </fieldset>
+  <fieldset><legend>Focus Groups</legend>
+    <input type="checkbox" name="group[]" id="group" value="business analytics"> Business Analytics<br>
+    <input type="checkbox" name="group[]" id="group" value="database design"> Database Design<br>
+    <input type="checkbox" name="group[]" id="group" value="microsoft office"> Microsoft Office<br>
+    <input type="checkbox" name="group[]" id="group" value="mobile applications"> Mobile Applications<br>
+    <input type="checkbox" name="group[]" id="group" value="network design"> Network Design<br>
+    <input type="checkbox" name="group[]" id="group" value="pc troubleshooting"> PC Troubleshooting/Support<br>
+    <input type="checkbox" name="group[]" id="group" value="security"> Security<br>
+    <input type="checkbox" name="group[]" id="group" value="system analysis"> System Analysis & Design<br>
+  </fieldset>
+  <input type="submit" name="submit" value="Submit">
+</form>
+
 <?php
 
 $name = $_POST['competion'];
 
 if(isset($_POST['competition'])) {
-  echo 'you chose';
+  echo 'you chose:<br />';
   foreach ($name as $c){
   echo $c."<br />";
 
