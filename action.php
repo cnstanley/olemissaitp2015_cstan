2b34d2b2bff1eda182b6aaf59ebf53585cd8f6fa
mpgoodwin <mpgoodwi@go.olemiss.edu>
2015-10-02 13:25:37 -0500

    updated reg, added actioned

---
+++ action.php
@@ -0,0 +1,18 @@
+<html>
+<title>Action</title>
+
+<?php
+
+$name = $_GET['color'];
+
+// optional
+// echo "You chose the following color(s): <br>";
+
+foreach ($c as $competition){
+echo $c."<br />";
+
+}
+
+ ?>
+
+</html>
