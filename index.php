1a8a587333ca1d97ae58b82999701ca2c1ee806b
mpgoodwin <mpgoodwi@go.olemiss.edu>
2015-10-02 16:08:50 -0500

    ready for group sql

--- index.php
+++ index.php
@@ -60,6 +60,13 @@ if(!empty($_POST)) {
       }
   }
 }
+if(!empty($_POST['check_list'])) {
+    foreach($_POST['check_list'] as $check) {
+            echo $check; //echoes the value set in the HTML form for each checked checkbox.
+                         //so, if I were to check 1, 3, and 5 it would echo value 1, value 3, value 5.
+                         //in your case, it would echo whatever $row['Report ID'] is equivalent to.
+    }
+}
 
 $Registration->showRegistrants($conn);
 ?>
