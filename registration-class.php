81e12fff3f201d0e8eea52bc92a55238cec2c825
elmo911 <jacobmwolfe@gmail.com>
2015-10-02 14:50:12 -0500

    v

--- registration-class.php
+++ registration-class.php
@@ -5,8 +5,6 @@
 class Registration
 {
 
-  public $conn;
-
   public function connectToDB()
   {
     $host = "us-cdbr-azure-southcentral-e.cloudapp.net";
@@ -43,6 +41,19 @@ class Registration
 
   public function showRegistrants()
   {
+    $host = "us-cdbr-azure-southcentral-e.cloudapp.net";
+    $user = "b59e50473555e9";
+    $pwd = "26114531";
+    $db = "acsm_0e7ddff7b7d920f";
+    try {
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e){
+        die(var_dump($e));
+    }
+
+    //
     $sql_select = "SELECT * FROM user_tbl";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll();
