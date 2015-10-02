01ba17838fe71ec08d84256b9ad8b703ad06a585
mpgoodwin <mpgoodwi@go.olemiss.edu>
2015-10-02 14:33:31 -0500

    renamed values

--- action.php
+++ action.php
@@ -2,11 +2,22 @@
 <title>Action</title>
 
 <form action="action.php" method="post">
-<input type="checkbox" name="check_list[]" value="value 1">one<br>
-<input type="checkbox" name="check_list[]" value="value 2"> two<br>
-<input type="checkbox" name="check_list[]" value="value 3"> three<br>
-<input type="checkbox" name="check_list[]" value="value 4"> four<br>
-<input type="checkbox" name="check_list[]" value="value 5"> five<br>
+  <fieldset><legend>Competitions</legend>
+    <input type="checkbox" name="check_list[]" value="National Collegiate Competition"> National Collegiate Competition<br>
+    <input type="checkbox" name="check_list[]" value="Regionals"> Regionals<br>
+  </fieldset>
+  <fieldset><legend>Focus Groups</legend>
+    <input type="checkbox" name="check_list[]" value="Database Design"> Database Design<br>
+    <input type="checkbox" name="check_list[]" value="Microsoft Office"> Microsoft Office<br>
+    <input type="checkbox" name="check_list[]" value="Mobile Applications"> Mobile Applications<br>
+    <input type="checkbox" name="check_list[]" value="Network Design"> Network Design<br>
+    <input type="checkbox" name="check_list[]" value="PC Troubleshooting/Support"> PC Troubleshooting/Support<br>
+    <input type="checkbox" name="check_list[]" value="Security"> Security<br>
+    <input type="checkbox" name="check_list[]" value="System Analysis & Design"> System Analysis & Design<br>
+  </fieldset>
+  <fieldset><legend>Other</legend>
+    <input type="checkbox" name="check_list[]" value="Website"> Website<br>
+  </fieldset>
 <input type="submit" />
 </form>
 <?php
