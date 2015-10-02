9cc4513e6ebcf0e316e4da944f50628e79452b92
elmo911 <jacobmwolfe@gmail.com>
2015-10-02 12:38:12 -0500

    connect attempt 1
    (plz work)

--- index.php
+++ index.php
@@ -24,7 +24,21 @@
       <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
-
+// DB connection info
+$host = "us-cdbr-azure-southcentral-e.cloudapp.net";
+$user = "b59e50473555e9";
+$pwd = "26114531";
+$db = "acsm_0e7ddff7b7d920f";
+// Connect to database.
+try {
+    $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    echo "<p> connected </p>";
+}
+catch(Exception $e){
+    die(var_dump($e));
+    echo "<p> not connected... </p>";
+}
 ?>
 </body>
 </html>
