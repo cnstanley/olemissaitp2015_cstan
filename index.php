c21b573ffa8f3a0dcf51fb828c419d3ffdf88e5e
elmo911 <jacobmwolfe@gmail.com>
2015-10-02 12:59:14 -0500

    Read db

--- index.php
+++ index.php
@@ -58,6 +58,23 @@ catch(Exception $e) {
 }
 echo "<h3>Your're registered!</h3>";
 }
+
+$sql_select = "SELECT * FROM user_tbl";
+$stmt = $conn->query($sql_select);
+$registrants = $stmt->fetchAll();
+if(count($registrants) > 0) {
+    echo "<h2>People who are registered:</h2>";
+    echo "<table>";
+    echo "<tr><th>Name</th>";
+    echo "<th>Email</th>";
+    foreach($registrants as $registrant) {
+        echo "<tr><td>".$registrant['name']."</td>";
+        echo "<td>".$registrant['email']."</td>";
+    }
+    echo "</table>";
+} else {
+    echo "<h3>No one is currently registered.</h3>";
+}
 ?>
 </body>
 </html>
