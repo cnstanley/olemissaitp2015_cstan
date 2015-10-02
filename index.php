9e7bded414991e5159d56856dba7e1aa3e2b6b78
mpgoodwin <mpgoodwi@go.olemiss.edu>
2015-10-02 14:52:27 -0500

    for each checkbox
    for each selected checkbox
    ready for sql

--- index.php
+++ index.php
@@ -47,9 +47,9 @@
 <?php
 if(!empty($_POST['check_list'])) {
     foreach($_POST['check_list'] as $check) {
-            echo $check; //echoes the value set in the HTML form for each checked checkbox.
-                         //so, if I were to check 1, 3, and 5 it would echo value 1, value 3, value 5.
-                         //in your case, it would echo whatever $row['Report ID'] is equivalent to.
+            echo $check; //echos the values from the checkboxes that are selected
+            //sql goes here
+            //insert $userid, $check into table
     }
 }
 ?>
