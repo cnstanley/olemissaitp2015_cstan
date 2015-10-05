80b2d89f319fd0c7b8ec5374e1723a7311a847c8
mpgoodwin <mpgoodwi@go.olemiss.edu>
2015-10-05 16:23:57 -0500

    functions done.

--- group-class.php
+++ group-class.php
@@ -2,17 +2,39 @@
 
 class Group{
 
-  $group_name;
-  $GID;
+  private $name;
+  private $ID;
 
-  public Group(){
-
+  public Group($n, $GID){
+    $this->setName($n);
+    $this->setID($GID);
   }
   public funtion getID(){
     return $this->GID;
   }
-  public function setID($id){
-    
+  public function setID($gid){
+    $this->ID = $gid;
+  }
+  public function getName(){
+    return $this->name;
+  }
+  public function setName($n){
+    $this->name = $n;
+  }
+  public function addToGroupTbl(){
+    try {
+        // Insert data
+        $sql_insert = "INSERT INTO group_tbl (name)
+                       VALUES (?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $this->name);
+        $stmt->execute();
+        return true;
+    }
+    catch(Exception $e) {
+        die(var_dump($e));
+        return false;
+    }
   }
 
 }
