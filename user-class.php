85e34a5a5c3c667a06e3e3ece7ea6b1e77713941
mpgoodwin <mpgoodwi@go.olemiss.edu>
2015-10-05 15:29:56 -0500

    User Class

---
+++ user-class.php
@@ -0,0 +1,47 @@
+<?php
+
+public class User{
+  private $conn; //connection variable to access database
+  private $name;
+  private $email;
+  public User($name1, $email1){
+    $this->setUser($name1, $email1);
+  }
+  public setConn($c){
+    $this->conn = $c;
+  }
+  private function setName($s){
+    $this->name = $s;
+  }
+  public function getName(){
+    return $name;
+  }
+  private function setEmail($s){
+    $this->email = $s;
+  }
+  public function getEmail(){
+    return $email;
+  }
+  public function setUser($n, $e){
+    $this->setName($n);
+    $this->setEmail($e);
+  }
+  public function saveToDB(){
+    try {
+        // Insert data
+        $sql_insert = "INSERT INTO user_tbl (name, email)
+                       VALUES (?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $this->name);
+        $stmt->bindValue(2, $this->email);
+        $stmt->execute();
+    }
+    catch(Exception $e) {
+        die(var_dump($e));
+    }
+  }
+
+
+}
+
+ ?>
