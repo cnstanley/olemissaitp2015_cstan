f8d2e860dfabb4aff40229d30e741a3fb0ce30f6
mpgoodwin <mpgoodwi@go.olemiss.edu>
2015-10-05 13:18:58 -0500

    First try at php functions
    wasn't sure about the column names in the database. functions added are
    marked with
    //needs to be tested

--- registration-class.php
+++ registration-class.php
@@ -7,6 +7,14 @@ class Registration
 
   public function addToUserTbl($conn, $name, $email)
   {
+    /*
+    //to be replaced with
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    addMemberToDB($conn, $name, $email);
+    foreach (group in selected_groups)
+      addMemberToGroup($conn, $email, group);
+    */
     try {
         // Insert data
         $sql_insert = "INSERT INTO user_tbl (name, email)
@@ -38,6 +46,33 @@ class Registration
     }
   }
 
+  public function addMemberToDb($conn, $name, $email){
+    //needs to be tested
+    try {
+      $sql_insert = "INSERT INTO user_tbl (name, email)
+                     VALUES (?,?)";
+      $stmt = $conn->prepare($sql_insert);
+      $stmt->bindValue(1, $name);
+      $stmt->bindValue(2, $email);
+      $stmt->execute();
+    } catch (Exception $e) {
+      die(var_dump($e));
+    }
+  }
+  public function addMemberToGroup($conn, $email, $group){
+    //needs to be tested
+    try {
+      $sql_insert = "INSERT INTO group_tbl (email, gname)
+                     VALUES (?,?)";
+      $stmt = $conn->prepare($sql_insert);
+      $stmt->bindValue(1, $email);
+      $stmt->bindValue(2, $group);
+      $stmt->execute();
+    } catch (Exception $e) {
+      die(var_dump($e));
+    }
+  }
+
   public function showRegistrants($conn)
   {
     $sql_select = "SELECT * FROM user_tbl";
@@ -50,7 +85,26 @@ class Registration
         echo "<th>Email</th>";
         foreach($registrants as $registrant) {
             echo "<tr><td>".$registrant['name']."</td>";
-            echo "<td>".$registrant['email']."</td>";
+            echo "<td>".$registrant['email']."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "<h3>No one is currently registered.</h3>";
+    }
+  }
+  public function showGroups($conn){
+    //needs to be tested
+    $sql_select = "SELECT * FROM group_tbl";
+    $stmt = $conn->query($sql_select);
+    $registrants = $stmt->fetchAll();
+    if(count($registrants) > 0) {
+        echo "<h2>Groups with people registered:</h2>";
+        echo "<table>";
+        echo "<tr><th>Name</th>";
+        echo "<th>Email</th>";
+        foreach($registrants as $registrant) {
+            echo "<tr><td>".$registrant['email']."</td>";
+            echo "<td>".$registrant['gname']."</td></tr>";//group column name?
         }
         echo "</table>";
     } else {
