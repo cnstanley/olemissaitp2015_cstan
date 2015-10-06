d06b50fd2b2f0e47ce8d41908b9a9d2df1a933a9
elmo911 <jacobmwolfe@gmail.com>
2015-10-05 19:57:41 -0500

    Removed unused code

--- registration-class.php
+++ registration-class.php
@@ -4,68 +4,6 @@
  */
 class Registration
 {
-
-  public function addToUserTbl($conn, $name, $email)
-  {
-    try {
-        // Insert data
-        $sql_insert = "INSERT INTO user_tbl (name, email)
-                       VALUES (?,?)";
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $name);
-        $stmt->bindValue(2, $email);
-        $stmt->execute();
-    }
-    catch(Exception $e) {
-        die(var_dump($e));
-    }
-    echo "<h3>You're Signed Up!</h3>";
-  }
-
-  public function addToSignedUpTbl($conn, $name, $group)
-  {
-    try {
-        // Insert data
-        $sql_insert = "INSERT INTO signed_up_tbl (gname, uname)
-                       VALUES (?,?)";
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $group);
-        $stmt->bindValue(2, $name);
-        $stmt->execute();
-    }
-    catch(Exception $e) {
-        die(var_dump($e));
-    }
-  }
-
-  public function addMemberToDb($conn, $name, $email){
-    //needs to be tested
-    try {
-      $sql_insert = "INSERT INTO user_tbl (name, email)
-                     VALUES (?,?)";
-      $stmt = $conn->prepare($sql_insert);
-      $stmt->bindValue(1, $name);
-      $stmt->bindValue(2, $email);
-      $stmt->execute();
-    } catch (Exception $e) {
-      die(var_dump($e));
-    }
-  }
-  //comment
-  public function addMemberToGroup($conn, $email, $group){
-    //needs to be tested
-    try {
-      $sql_insert = "INSERT INTO group_tbl (email, gname)
-                     VALUES (?,?)";
-      $stmt = $conn->prepare($sql_insert);
-      $stmt->bindValue(1, $email);
-      $stmt->bindValue(2, $group);
-      $stmt->execute();
-    } catch (Exception $e) {
-      die(var_dump($e));
-    }
-  }
-
   public function showRegistrants($conn)
   {
     $sql_select = "SELECT * FROM user_tbl";
@@ -79,25 +17,6 @@ class Registration
         foreach($registrants as $registrant) {
             echo "<tr><td>".$registrant['name']."</td>";
             echo "<td>".$registrant['email']."</td></tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "<h3>No one is currently registered.</h3>";
-    }
-  }
-  public function showGroups($conn){
-    //needs to be tested
-    $sql_select = "SELECT * FROM group_tbl";
-    $stmt = $conn->query($sql_select);
-    $registrants = $stmt->fetchAll();
-    if(count($registrants) > 0) {
-        echo "<h2>Groups with people registered:</h2>";
-        echo "<table>";
-        echo "<tr><th>Name</th>";
-        echo "<th>Email</th>";
-        foreach($registrants as $registrant) {
-            echo "<tr><td>".$registrant['email']."</td>";
-            echo "<td>".$registrant['gname']."</td></tr>";//group column name?
         }
         echo "</table>";
     } else {
